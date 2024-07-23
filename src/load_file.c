b22e16ac092f995333c63987cbb48ceb9ea70f41
Michael lemito <delovoy.lemit@gmail.com>
2024-07-23 20:11:38 +0300

    Добавил чтение с файла

---
+++ src/load_file.c
@@ -0,0 +1,23 @@
+#include "pdp.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+void load_file(char* filename){
+  FILE* input_file = fopen(filename, "r");
+
+  if (input_file == NULL){
+    perror("data.txt");
+    exit(errno);
+  }
+
+  address start;
+  int N;
+  byte input;
+
+  while (fscanf(input_file, "%hx %x", &start, &N) == 2) {
+    for (int i = 0; i < N; ++i) {
+      fscanf(input_file, "%hhx", &input);
+      b_write(start + i, input);
+    }
+  }
+}
